07c1a26db81c24ad993570eed73e7849e879c546
Abhishek Kumar Verma <ruhul10101@gmail.com>
2023-07-16 11:05:41 +0530

    initial commit

---
+++ Binary_Search.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def binary_search(arr,key,low,high):
+    
+    if low>high:
+        return -1
+    
+    mid = low + ((high-low)//2)  
+    
+    
+    if arr[mid] == key:
+        
+        return mid
+    elif key<arr[mid]: 
+        
+        return binary_search(arr, key, low, mid-1)
+    else:
+        return binary_search(arr, key, mid+1, high)
+        
+        
+a=[2,4,5,6,7,8,9]
+
+b=binary_search(a, 9, 0, 6)
+print(b)
